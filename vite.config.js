e19e262ea73ad73c3ff44e7e4dd9d0f07f0e4513
brandon-almanza <brandonalmanza2109@gmail.com>
2025-09-27 00:13:02 -0400

    first commit (WORK IN PROGRESS)

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/brandon-portfolio/",
+  base: "/brandon-almanza-portfolio/",
 })
